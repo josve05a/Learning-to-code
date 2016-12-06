65c556fc350d754513b826ba96c2fbd231674b7f
Jonatan Svensson Glad <josve05a@users.noreply.github.com>
2016-12-06 20:48:22 +0100

    Update test.js

--- Public-Domain-Checker/test.js
+++ Public-Domain-Checker/test.js
@@ -1,5 +1,8 @@
 var country	=	prompt ("In which country was this file first published? (ISO-code, lower case)");
-var death	=	prompt ("Which year did the author of the work die? If alive type FALSE");
+var dead	=	prompt ("Is the author of this work dead? y/n");
+	if dead (y) {
+		var death	=	prompt ("Which year did the author of the work die? If alive type FALSE");	
+	}
 var publication	=	prompt ("In which year was this first published?");
 
 switch (country) {

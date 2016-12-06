fac5ce5698a2be9a695be6d2b7e7ac5254da46c9
Jonatan Svensson Glad <josve05a@users.noreply.github.com>
2016-12-06 21:11:29 +0100

    Update test.js

--- Public-Domain-Checker/test.js
+++ Public-Domain-Checker/test.js
@@ -1,3 +1,4 @@
+//functions
 function deathFunction () {
 	var death = prompt ("Which year did the author of the work die?");
 }
@@ -5,9 +6,22 @@ function publicationFunction () {
 	var published = prompt ("Which year was this work published?");
 }
 
-
+// variables
 var country = prompt ("In which country was this file first published? (ISO-code, lower case)");
 
+var dead = prompt ("Is the author of this work dead? y/n");
+	if (dead == "y" || "yes") {
+		deathFunction();
+	}
+	if (dead != "y" || "n" || "yes" || "no") {
+		"You did something wrong!"
+	}
+var publication = prompt ("Is the publication year of this work known? y/n");
+	if (publication = "y" || "yes") {
+		publicationFunction();	
+	}
+
+// countries
 switch (country) {
 	case "us":
 		UnitedStates();
@@ -16,14 +30,3 @@ switch (country) {
 		"Sorry, this hasn't been coded yet."
 	break;
 }
-
-var dead = prompt ("Is the author of this work dead? y/n");
-	if (dead == "y" || "yes") {
-		deathFunction();
-	}
-var publication = prompt ("Is the publication year of this work known? y/n");
-	if (publication = "y" || "yes") {
-		publicationFunction();	
-	}
-
-

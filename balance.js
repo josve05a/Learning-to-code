49cd948970cac399722be7585745faf98107e1fd
Jonatan Svensson Glad <josve05a@users.noreply.github.com>
2016-12-06 19:57:06 +0100

    complicating code to all be nested

--- balance.js
+++ balance.js
@@ -2,12 +2,14 @@ var balance = prompt("How much money do you got?");
 
 if (balance < 0) {
 	alert("You have negative money! Welp!");
-}
+} else {
 
 if (balance == 0) {
 	alert("You have no cash brotha!");
-}
+} else {
 
 if (balance > 0) {
 	alert("You have got money! Woop-woop!");
 }
+	}
+	}

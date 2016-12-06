4bf662bcd88c68b4392da44a5fc29816fcc7f207
Jonatan Svensson Glad <josve05a@users.noreply.github.com>
2016-12-06 19:55:26 +0100

    refactored code to be less nested

--- balance.js
+++ balance.js
@@ -1,10 +1,13 @@
 var balance = prompt("How much money do you got?");
-if (balance <= 0) {
-  if (balance == 0) {
-    alert("You have no cash brotha!");
-  } else {
-    alert("You have negative money! Welp!");
-  }
-} else {
-  alert("You have got money! Woop-woop!");
+
+if (balance < 0) {
+	alert("You have negative money! Welp!");
+}
+
+if (balance == 0) {
+	alert("You have no cash brotha!");
+}
+
+if (balance > 0) {
+	alert("You have got money! Woop-woop!");
 }

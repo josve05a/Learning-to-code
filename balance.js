73710ac313e8dc414c42b08931a96064e1921915
Jonatan Svensson Glad <josve05a@users.noreply.github.com>
2016-12-06 20:00:02 +0100

    Update balance.js

--- balance.js
+++ balance.js
@@ -3,13 +3,10 @@ var balance = prompt("How much money do you got?");
 if (balance < 0) {
 	alert("You have negative money! Welp!");
 } else {
-
-if (balance == 0) {
-	alert("You have no cash brotha!");
-} else {
-
-if (balance > 0) {
-	alert("You have got money! Woop-woop!");
+	if (balance == 0) {
+		alert("You have no cash brotha!");} else {
+			if (balance > 0) {
+				alert("You have got money! Woop-woop!");
+			}
+		}
 }
-	}
-	}

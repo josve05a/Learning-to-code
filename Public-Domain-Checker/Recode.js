99c4d6824ad6ff65accdc7405671ab3333838567
Jonatan Svensson Glad <josve05a@users.noreply.github.com>
2016-12-08 11:26:19 +0100

    Update Recode.js

--- Public-Domain-Checker/Recode.js
+++ Public-Domain-Checker/Recode.js
@@ -7,11 +7,21 @@ function killFunction () {
 // variables
 var multipleValues = [];
 var published;
+var deathyear;
 
 // questions
 multipleValues[0] = prompt("Was this published before 1923? y/n")
+multipleValues[1] = prompt("Which year did the author die? If the author is alive, type ALIVE")
 
 // template creation
 if (multipleValues[0] === "y" || multipleValues[0] === "yes") {
-published = "-1923"
+	published = "-1923"
+}
+
+if (multipleValues[1] === "alive" || multipleValues[1] === "ALIVE") {
+	deathyear = ""
+}
+
+if (multipleValues[1].isNumber) {
+	alert ("It is a number. Congratulations.")
 }

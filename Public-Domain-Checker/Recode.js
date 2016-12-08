37e8bcb65719d1c2cc5ffbce4baf205e0ca80393
Jonatan Svensson Glad <josve05a@users.noreply.github.com>
2016-12-08 11:59:09 +0100

    Update Recode.js

--- Public-Domain-Checker/Recode.js
+++ Public-Domain-Checker/Recode.js
@@ -2,10 +2,23 @@
 var multipleValues = [];
 var published;
 var deathyear;
+var art;
+var old;
+
+// killing the tool
+function killFunction () {
+	alert("Sorry, this")
+	throw new Error("Sorry, this hasn't been coded yet.")
+}
+
+// this year
+var d = new Date();
+var n = d.getFullYear();
 
 // questions
 multipleValues[0] = prompt("Was this published before 1923? y/n")
 multipleValues[1] = prompt("Which year did the author die? If the author is alive, type ALIVE")
+multipleValues[2] = prompt("Is this work a faithful reproduction of a 2D-work (e.g. painting)? y/n")
 
 // template creation
 if (multipleValues[0] === "y" || multipleValues[0] === "yes") {
@@ -18,4 +31,13 @@ if (multipleValues[1] === "alive" || multipleValues[1] === "ALIVE") {
 	deathyear = "|deathyear=" + multipleValues[1]	
 }
 
-alert("{{PD-old-auto" + published + deathyear + "}}");
+if (multipleValues[1] <= (n - 70)) {
+	old = "PD-old-auto"
+}
+
+if (multipleValues[2] === "y" || multipleValues[2] === "yes") {
+	art = "PD-Art|"
+}
+
+//result
+alert("{{" + art + old + published + deathyear + "}}");
